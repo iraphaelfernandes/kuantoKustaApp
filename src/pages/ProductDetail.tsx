42250b96afd3ef77660207bb03eca75a822c7e44
Raphael Fernandes <raphael.fernandes.eng@gmail.com>
2023-03-05 18:53:32 -0300

    Fix url

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -26,7 +26,7 @@ export default function ProductDetail() {
         <div className="py-6">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center space-x-2 text-gray-400 text-sm">
-              <Link to="/shop">
+              <Link to="/">
                 <a className="hover:underline hover:text-gray-600">Shop</a>
               </Link>
               <span>
